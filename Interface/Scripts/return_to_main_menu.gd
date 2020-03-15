20fae0665bc650ddabd8780958c1880e79b5d27b
Viktor Velev <viktorvelev8@gmail.com>
2020-03-15 20:52:00 +0100

    Added simple Menu abstractions and fade_in animation

---
+++ Interface/Scripts/return_to_main_menu.gd
@@ -0,0 +1,7 @@
+extends Control
+
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Interface/TitleScreen.tscn")
+	pass # Replace with function body.
---
+++ Interface/routing.gd
@@ -0,0 +1,18 @@
+extends Control
+
+var scene_to_load
+
+func _ready():
+	$Menu/Controls/PlayButton.grab_focus()
+	for button in $Menu/Controls.get_children():
+		button.connect("pressed", self, "_on_pressed", [button.scene_to_load])
+
+func _on_pressed(_scene_path):
+	scene_to_load = _scene_path
+	print("Changing scene to " + scene_to_load)
+	$FadeIn.show()
+	$FadeIn.fade_in()
+
+# Actually change the scene once the transition is finished.
+func _on_FadeIn_fade_finished():
+	get_tree().change_scene(scene_to_load)
